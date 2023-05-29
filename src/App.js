de2161b3da8dd0a89b11e002a9c4a9499c14f5b0
Nikhil Savalgi <nicck008@gmail.com>
2023-05-29 17:45:40 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Youtube</h1>
+      <Navbar/>
     </div>
   );
 }
